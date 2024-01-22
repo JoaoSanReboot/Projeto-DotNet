bbb36c0df8c7685498e1282cd7db5f266970cebc
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2024-01-22 20:53:01 -0300

    NewProject.exe

---
+++ ServidorHttpSimples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ServidorHttpsSimples
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servidorHttp = new ServidorHttp();
+        }
+
+    }
+}
